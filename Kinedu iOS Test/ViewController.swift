ea7b128bb88803dd98aec00c0a9778e6c6dc70f9
Carlos Quiroga <cquiroga@multinfmty.com>
2019-10-22 11:01:57 -0500

    Initial Commit

---
+++ Kinedu iOS Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kinedu iOS Test
+//
+//  Created by Developer on 10/22/19.
+//  Copyright © 2019 Appsodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kinedu iOS TestTests/Kinedu_iOS_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kinedu_iOS_TestTests.swift
+//  Kinedu iOS TestTests
+//
+//  Created by Developer on 10/22/19.
+//  Copyright © 2019 Appsodi. All rights reserved.
+//
+
+import XCTest
+@testable import Kinedu_iOS_Test
+
+class Kinedu_iOS_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
